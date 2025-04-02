bb1bc0b95683b25ff369232e98d5e96b4aa94492
S33BAS <sebastian2005tf@hotmail.com>
2025-04-02 00:22:11 -0500

    capas, movimiento, variables y animaciones
    1-	Layers
    •	Se agregaron el total de 5 capas (Player, Ground, BacGround, FrontGround, Enemies).
    2-	Movimiento
    •	Se crearon x metodos
    I.	“Movement” Hasta el momento contiene el movimiento horizontal y animación del salto
    II.	“FlipCharacter” Este contiene la orientación horizontal del personaje en pantalla
    III.	“FixedUpdate” Este se coloca los métodos que se ejecutan en tiempos fijos, normalmente las físicas del juego
    IV.	“Jump”
    3-	Se creo un GameObject “GroundCheck”, este tiene la función de decirnos si el personaje se encuentra en el “Ground”
    4-	Variables
    •	Se agregaron 6 variables
    I.	JumpHeight
    II.	groundCheck
    III.	IsGrounded
    IV.	groundCheckRadius
    V.	WhatIsGround
    VI.	Anim – ComponenteAnimator
    5-	Animación
    •	Se crearon las animaciones de Idle, Run y Jump al personaje
    6-	Carpeta
    •	Se creo una carpeta nueva en assets “Animation”, esta es en donde unity unirá cada spray y donde el componente Animator buscará dichas animaciones

--- Assets/2. Scripts/PlayerController.cs
+++ Assets/2. Scripts/PlayerController.cs
@@ -3,22 +3,76 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;
+    public float speed, JumpHeight;
     float velX, velY;
     Rigidbody2D rb;
+    public Transform groundcheck;
+    public bool isGrounded;
+    public float groundCheckRadius;
+    public LayerMask WhatIsGround;
+    Animator anim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundcheck.position, groundCheckRadius, WhatIsGround);
+
+        if (isGrounded)
+        {
+            anim.SetBool("Jump", false);
+        }
+        else
+        {
+            anim.SetBool("Jump", true);
+        }
+
+        FlipCharacter();
+    }
+    private void FixedUpdate()
+    {
+        Movement();
+        Jump();
+    }
+    public void Jump()
+    {
+        if (Input.GetButton("Jump") && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, JumpHeight);
+        }
+    }
+    public void Movement()
+    {
         velX = Input.GetAxisRaw("Horizontal");
         velY = rb.linearVelocity.y;
 
         rb.linearVelocity = new Vector2(velX * speed, velY);
+
+        if (rb.linearVelocity.x != 0)
+        {
+            anim.SetBool("Run", true);
+        }
+        else
+        {
+            anim.SetBool("Run", false);
+        }
+    }
+
+    public void FlipCharacter()
+    {
+        if (rb.linearVelocity.x >= 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
     }
 }
