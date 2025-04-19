fd3c251ea1b3cc6c74f7c80a4b28aa7840c02ccd
S33BAS <sebastian2005tf@hotmail.com>
2025-04-18 20:19:57 -0500

    AVANCE COMBATE Y LOGICA DE VIDA PARA ENEMIGOS Y JUGADOR
    Se agregó el sistema de vida y daño para enemigos y el jugador.
    
    Implementadas animaciones de recibir daño y muerte.
    
    Se incorporó lógica de knockback al recibir daño.
    
    Se añadieron variables configurables para fuerza de empuje y daño por enemigo.
    
    El jugador ya no puede recibir daño ni moverse tras morir.
    
    Mejoras generales en organización de scripts y lógica de colisiones.
    
    Ajustes de escala y dimensiones en escena para mantener proporciones correctas.

---
+++ Assets/2. Scripts/Enemy/Enemy.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public string Enemyname;
+    public float speed;
+    public float healtPoints;
+    public float damageToGive;
+    public float knockbackForce;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
