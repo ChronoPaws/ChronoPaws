99dffa42451a43906cbfe0ffb50508ee5d7190d2
Diogenes Celis Soto <diogenescelis98@gmail.com>
2025-04-30 15:55:15 -0500

    Sistema de Pausa
    Se ajusto el sistema de pausa del juego y las interacciones entre las escenas y paneles correspondientes asi como sus scripts.
    
    adicional se creadron y ajustaron nuevas animaciones del player para tener a consideracion a futuro

--- Assets/2. Scripts/UI Interface/Pausar Juego.cs
+++ Assets/2. Scripts/UI Interface/Pausar Juego.cs
@@ -1,9 +1,10 @@
-using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PausarJuego : MonoBehaviour
 {
     public GameObject menuPausa;
+    public GameObject menuConfirmarSalida;
     public bool juegoPausado = false;
 
     private void Update()
@@ -13,29 +14,48 @@ public class PausarJuego : MonoBehaviour
             if (juegoPausado)
             {
                 Reanudar();
-
             }
             else
             {
                 Pausar();
             }
-
-
         }
     }
 
-        public void Reanudar()
+    public void Reanudar()
     {
         menuPausa.SetActive(false);
-        Time.timeScale = 1;
+        Time.timeScale = 1f;
         juegoPausado = false;
     }
 
     public void Pausar()
     {
         menuPausa.SetActive(true);
-        Time.timeScale = 0;
+        Time.timeScale = 0f;
         juegoPausado = true;
     }
 
+    public void IrAlMenuPrincipal()
+    {
+        Time.timeScale = 1; // Restaurar el tiempo antes de cambiar de escena
+        SceneManager.LoadScene("1. Menu");
+    }
+
+    public void ConfirmarSalida()
+    {
+        menuConfirmarSalida.SetActive(true); // Muestra el panel de confirmación
+
+    }
+
+    public void CancelarSalida()
+    {
+        menuConfirmarSalida.SetActive(false); // Oculta el panel de confirmación
+    }
+
+    public void SalirDelJuego()
+    {
+        Debug.Log("Saliste del juego");
+        Application.Quit(); // Esto solo funciona fuera del editor
+    }
 }
